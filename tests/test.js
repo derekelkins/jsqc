c82b7108d6554a7ef520fa61553753c4d126fe8c
Derek Elkins <derek.a.elkins@gmail.com>
2017-08-16 07:22:42 -0700

    Upgrade to QUnit 2.4.0.

--- tests/test.js
+++ tests/test.js
@@ -19,64 +19,67 @@ var TR = QC.TestRunner.Default;
 var ShortTR = new QC.TestRunner({ maxSuccesses: 10, maxDiscards: 10 });
 var Gen = QC.Gen;
 var Stream = QC.Stream;
-var equiv = QUnit.equiv;
 
-function qc(gens, body) {
-    TR.testRun(Gen.forAllNoShrink(gens, body), ok);
+function qc(assert, gens, body) {
+    TR.testRun(Gen.forAllNoShrink(gens, body), assert.ok.bind(assert));
 }
 
-module('RandGen');
+QUnit.module('RandGen');
 
-test('random calls differ', function() {
+QUnit.test('random calls differ', function(assert) {
     var rng = new QC.RandGen(new (Uint32Array || Array)(10));
-    notEqual(rng.randomUint(), rng.randomUint());
+    assert.notEqual(rng.randomUint(), rng.randomUint());
 });
 
-module('Stream');
+QUnit.module('Stream');
 
-test('toArray o fromArray', function() {
-    qc([Gen.genInt.arrayOf().resized(5)], function(xs) {
-        return equiv(Stream.fromArray(xs).toArray(), xs);
+QUnit.test('toArray o fromArray', function(assert) {
+    qc(assert, [Gen.genInt.arrayOf().resized(5)], function(xs) {
+        assert.deepEqual(Stream.fromArray(xs).toArray(), xs);
+        return true;
     });
 });
 
-test('concat two', function() {
-    qc([Gen.genInt.arrayOf().resized(5), Gen.genInt.arrayOf().resized(5)], function(x, y) {
+QUnit.test('concat two', function(assert) {
+    qc(assert, [Gen.genInt.arrayOf().resized(5), Gen.genInt.arrayOf().resized(5)], function(x, y) {
         var ss = Stream.fromArray([Stream.fromArray(x), Stream.fromArray(y)]);
-        return equiv(ss.flatten().toArray(), x.concat(y));
+        assert.deepEqual(ss.flatten().toArray(), x.concat(y));
+        return true;
     });
 });
 
-test('concat unit', function() {
-    qc([Gen.genInt.arrayOf().resized(5)], function(x) {
+QUnit.test('concat unit', function(assert) {
+    qc(assert, [Gen.genInt.arrayOf().resized(5)], function(x) {
         var  leftUnit = Stream.fromArray([Stream.Empty, Stream.fromArray(x)]).flatten();
         var rightUnit = Stream.fromArray([Stream.fromArray(x), Stream.Empty]).flatten();
-        return equiv(leftUnit.toArray(), x) && equiv(rightUnit.toArray(), x);
+        assert.deepEqual(leftUnit.toArray(), x) && assert.deepEqual(rightUnit.toArray(), x);
+        return true;
     });
 });
 
-module('Gen');
+QUnit.module('Gen');
 
-test('typeof genBool is boolean', function() {
-    equal(typeof Gen.genBool(QC.StdRandGen, 100)[0], 'boolean');
+QUnit.test('typeof genBool is boolean', function(assert) {
+    assert.equal(typeof Gen.genBool(QC.StdRandGen, 100)[0], 'boolean');
 });
 
-test('shrinkArray', function() {
+QUnit.test('shrinkArray', function(assert) {
     // Note that the order of the results doesn't particularly matter, so this
     // test over-specifies the result.
-    deepEqual(Gen.shrinkArray(Gen.shrinkBool)([true, true]).toArray(),
+    assert.deepEqual(Gen.shrinkArray(Gen.shrinkBool)([true, true]).toArray(),
         [[false, true],[false,false],[true,false]]);
 });
 
-test('unit tests are a special case of QC tests: shrinkArray part deux', function() {
-    qc([], function() {
-        return equiv(Gen.shrinkArray(Gen.shrinkBool)([true, true]).toArray()
+QUnit.test('unit tests are a special case of QC tests: shrinkArray part deux', function(assert) {
+    qc(assert, [], function() {
+        assert.deepEqual(Gen.shrinkArray(Gen.shrinkBool)([true, true]).toArray()
                     ,[[false, true],[false,false],[true,false]]);
+        return true;
     });
 });
 
-test('chooseInt(lo,hi) between lo and hi', function() {
-    qc([Gen.genInt, Gen.genInt], function(a, b) {
+QUnit.test('chooseInt(lo,hi) between lo and hi', function(assert) {
+    qc(assert, [Gen.genInt, Gen.genInt], function(a, b) {
         var x = 0;
         if(a < b) {
             x = Gen.chooseInt(a, b)(QC.StdRandGen, 100);
@@ -88,44 +91,45 @@ test('chooseInt(lo,hi) between lo and hi', function() {
     });
 });
 
-module('TestRunner');
+QUnit.module('TestRunner');
 
-test('passing test', function() {
+QUnit.test('passing test', function(assert) {
     TR.testRun(Gen.forAllNoShrink([Gen.genBool, Gen.genBool], function(x, y) {
         return x === x;
-    }), function(b, s) { ok(b && s.indexOf('Passed') === 0, s) });
+    }), function(b, s) { assert.ok(b && s.indexOf('Passed') === 0, s) });
 });
 
-test('proved test', function() {
+QUnit.test('proved test', function(assert) {
     TR.testRun(Gen.forAllNoShrink([], function() {
         return true;
-    }), function(b, s) { ok(b && s.indexOf('Proved') === 0, s) });
+    }), function(b, s) { assert.ok(b && s.indexOf('Proved') === 0, s) });
 });
 
-test('failing test', function() {
+QUnit.test('failing test', function(assert) {
     TR.testRun(Gen.forAllNoShrink([Gen.genBool, Gen.genBool], function(x, y) {
         return x === y;
-    }), function(b, s) { ok(!b && s.indexOf('Failed') === 0, s); });
+    }), function(b, s) { assert.ok(!b && s.indexOf('Failed') === 0, s); });
 });
 
-test('exception test', function() {
+QUnit.test('exception test', function(assert) {
     TR.testRun(Gen.forAllNoShrink([Gen.genBool, Gen.genBool], function(x, y) {
         throw "foo";
-    }), function(b, s) { ok(!b && s.indexOf('Exception') === 0, s); });
+    }), function(b, s) { assert.ok(!b && s.indexOf('Exception') === 0, s); });
 });
 
-test('give up test', function() {
+QUnit.test('give up test', function(assert) {
     TR.testRun(Gen.forAllNoShrink([Gen.genBool.suchThat(function(b) { return false; }), Gen.genBool], function(x, y) {
         return x === x;
-    }), function(b, s) { ok(!b && s.indexOf('Undecided') === 0, s); });
+    }), function(b, s) { assert.ok(!b && s.indexOf('Undecided') === 0, s); });
 });
 
-asyncTest('async test', function() {
+QUnit.test('async test', function(assert) {
+    var done = assert.async();
     ShortTR.testRunAsync(function(c) { 
         return Gen.forAllNoShrinkAsync([Gen.genBool, Gen.genBool], function(k) { return function(x,y) {
             window.setTimeout(function() { k(x === x); }, 0);
         }; }, c)
-    }, function(b, s) { ok(b, s); start(); });
+    }, function(b, s) { assert.ok(b, s); done(); });
 });
 
 });
